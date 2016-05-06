ac7f180a53d04ea90bf3eb0f93ac16a97836a9bd
Hitesh Kumar <hitesh.kumar@snapdeal.com>
2016-05-06 12:06:22 +0530

    Initial Commit

---
+++ SwiftExapmleTests/SwiftExapmleTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftExapmleTests.swift
+//  SwiftExapmleTests
+//
+//  Created by Hitesh Kumar on 06/05/16.
+//  Copyright © 2016 Hitesh Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftExapmle
+
+class SwiftExapmleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
